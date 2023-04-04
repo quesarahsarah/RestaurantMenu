41730f0035042d757ef4a890c594b8b6e1ae99f1
quesarahsarah <quesarahsarah90@gmail.com>
2023-04-03 19:13:46 -0500

    For Studio 4: Created project and initialized fields for Menu and MenuItem classes

---
+++ src/main/java/restaurant/Menu.java
@@ -0,0 +1,10 @@
+package restaurant;
+
+import java.util.ArrayList;
+import java.util.Date;
+
+public class Menu {
+    private Date dateUpdated;
+    private ArrayList<MenuItem> items;
+    private MenuItem mostRecent;
+}
---
+++ src/main/java/restaurant/MenuItem.java
@@ -0,0 +1,17 @@
+package restaurant;
+
+import java.util.ArrayList;
+import java.util.Date;
+
+public class MenuItem {
+    private String name;
+    private double price;
+    private String description;
+    private String category;
+    private boolean isNew;
+    private boolean isVegan;
+    private ArrayList<String> allergens;
+    private Date dateAdded;
+    private int spiceLevel;
+    private String proteinType;
+}
